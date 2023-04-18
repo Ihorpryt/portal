5a4178840b572262f80f39a5a6efeed136db5713
Tarik El shaar <ihorgosha9@gmail.com>
2023-04-18 14:46:25 +0300

    caret

--- src/lib/images/icons/caret.svelte
+++ src/lib/images/icons/caret.svelte
@@ -3,13 +3,23 @@
 </script>
 
 <div class="icon">
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M7.40728 10.4534C7.68394 10.7493 8.1532 10.7493 8.42985 10.4534L12.147 6.47827C12.5653 6.03098 12.2481 5.30017 11.6357 5.30017H4.20139C3.58901 5.30017 3.27184 6.03098 3.6901 6.47827L7.40728 10.4534Z" fill="#6B7280"/>
-    </svg>                     
+    <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_2882_9142)">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M0.71967 0.619572C1.01256 0.326679 1.48744 0.326679 1.78033 0.619572L5 3.83924L8.21967 0.619572C8.51256 0.326679 8.98744 0.326679 9.28033 0.619572C9.57322 0.912465 9.57322 1.38734 9.28033 1.68023L5.53033 5.43023C5.23744 5.72313 4.76256 5.72313 4.46967 5.43023L0.71967 1.68023C0.426777 1.38734 0.426777 0.912465 0.71967 0.619572Z" fill="black"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_2882_9142">
+        <rect width="10" height="6" fill="white"/>
+        </clipPath>
+        </defs>
+    </svg>                            
 </div>  
 
 <style>
     .icon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         width: 16px !important;
         height: 16px;
     }
